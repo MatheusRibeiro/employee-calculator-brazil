1c1eb163a8c722276d9ead2acb0e37b996d11649
Matheus Ribeiro <matheus.salgado@gmail.com>
2020-10-29 20:27:27 -0300

    lint

--- src/dateHelper.js
+++ src/dateHelper.js
@@ -1,10 +1,9 @@
 const moment = require('moment')
 
 module.exports = {
-  completedYears: function completedYears(startDate, endDate) {
+  completedYears: function completedYears (startDate, endDate) {
     const startsAt = moment(`${startDate} 00:00`)
     const endsAt = moment(`${endDate} 23:59`)
     return endsAt.diff(startsAt, 'years')
   }
-
 }
