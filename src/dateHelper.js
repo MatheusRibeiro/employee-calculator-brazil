9d53553f364fbd327e6373ea8340a4992c9eb93c
Matheus Ribeiro <matheus.salgado@gmail.com>
2020-10-29 21:03:22 -0300

    adding some date method on its helper

--- src/dateHelper.js
+++ src/dateHelper.js
@@ -5,5 +5,26 @@ module.exports = {
     const startsAt = moment(`${startDate} 00:00`)
     const endsAt = moment(`${endDate} 23:59`)
     return endsAt.diff(startsAt, 'years')
+  },
+  completedMonthsFromAniversary: function completedMonthsFromAniversary (startDate, endDate) {
+    const startsAt = moment(`${startDate} 00:00`)
+    const endsAt = moment(`${endDate} 23:59`)
+    return endsAt.diff(startsAt, 'months') % 12
+  },
+
+  completedMonthsFromYear: function completedMonthsFromYear (referenceDate) {
+    const date = moment(referenceDate)
+    const month = date.month()
+    const day = date.date()
+
+    if (day >= 15) {
+      return month + 1
+    }
+    return month
+  },
+
+  completedDaysFromMonth: function completedDaysFromMonth (referenceDate) {
+    const date = moment(referenceDate)
+    return date.date()
   }
 }
--- src/dateHelper.test.js
+++ src/dateHelper.test.js
@@ -1,30 +1,83 @@
 describe('Date Helper', function () {
   const { expect } = require('chai')
 
-  const { completedYears } = require('./dateHelper')
+  const {
+    completedYears,
+    completedMonthsFromYear,
+    completedDaysFromMonth,
+    completedMonthsFromAniversary
+  } = require('./dateHelper')
 
-  const testValues = [
+  const dateDiffsInputs = [
     {
       startDate: '2013-09-13',
       endDate: '2020-11-04',
-      expectedCompletedYears: 7
-    },
-    {
-      startDate: '2019-11-04',
-      endDate: '2020-11-04',
-      expectedCompletedYears: 1
+      expectedCompletedYears: 7,
+      expectedMonthsAfterAniversary: 1
     },
     {
       startDate: '2019-11-05',
       endDate: '2020-11-04',
-      expectedCompletedYears: 0
+      expectedCompletedYears: 0,
+      expectedMonthsAfterAniversary: 11
+    },
+    {
+      startDate: '2019-11-05',
+      endDate: '2020-11-05',
+      expectedCompletedYears: 1,
+      expectedMonthsAfterAniversary: 0
+    },
+    {
+      startDate: '2018-10-05',
+      endDate: '2020-01-04',
+      expectedCompletedYears: 1,
+      expectedMonthsAfterAniversary: 2
     }
   ]
 
-  testValues.forEach(function ({ startDate, endDate, expectedCompletedYears }) {
+  dateDiffsInputs.forEach(function ({ startDate, endDate, expectedCompletedYears }) {
     it(`calculates the number of years completed between ${startDate} and ${endDate}`, function () {
       const years = completedYears(startDate, endDate)
       expect(years).to.equals(expectedCompletedYears)
+    })
+  })
+
+  dateDiffsInputs.forEach(function ({ startDate, endDate, expectedMonthsAfterAniversary }) {
+    it(`calculates the number of months completed after aniversary between ${startDate} and ${endDate}`, function () {
+      const years = completedMonthsFromAniversary(startDate, endDate)
+      expect(years).to.equals(expectedMonthsAfterAniversary)
+    })
+  })
+
+  const currentYearDiffs = [
+    {
+      referenceDate: '2010-01-07',
+      expectedDaysOnMonth: 7,
+      expectedMonthsInYear: 0
+    },
+    {
+      referenceDate: '2020-11-04',
+      expectedDaysOnMonth: 4,
+      expectedMonthsInYear: 10
+    },
+    {
+      referenceDate: '2020-09-23',
+      expectedDaysOnMonth: 23,
+      expectedMonthsInYear: 9
+    }
+  ]
+
+  currentYearDiffs.forEach(function ({ referenceDate, expectedMonthsInYear }) {
+    it(`calculates the number of months from year start for ${referenceDate}`, function () {
+      const months = completedMonthsFromYear(referenceDate)
+      expect(months).to.equals(expectedMonthsInYear)
+    })
+  })
+
+  currentYearDiffs.forEach(function ({ referenceDate, expectedDaysOnMonth }) {
+    it(`calculates the number of days from month start for ${referenceDate}`, function () {
+      const days = completedDaysFromMonth(referenceDate)
+      expect(days).to.equals(expectedDaysOnMonth)
     })
   })
 })
