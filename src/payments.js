2b12714dfb8f696326fa10d6dc510c26349a0a2c
Matheus Ribeiro <matheus.salgado@gmail.com>
2020-10-30 23:46:45 -0300

    function to calculate and get details of advance notice indemnities after termination

--- src/payments.js
+++ src/payments.js
@@ -2,7 +2,11 @@ const { INSS } = require('./inss')
 const { IRRF } = require('./irrf')
 const { roundCurrency } = require('./currencyHelper')
 
-const { completedDaysFromMonth } = require('./dateHelper')
+const {
+  addDays,
+  completedYears,
+  completedDaysFromMonth
+} = require('./dateHelper')
 
 function salaryRemainer ({ grossSalary, endDate }) {
   const daysForSalaryRemainer = completedDaysFromMonth(endDate)
@@ -25,6 +29,34 @@ function salaryRemainer ({ grossSalary, endDate }) {
   }
 }
 
+function advanceNoticeSalary ({ grossSalary, startDate, endDate }) {
+  const days = advanceNoticeDays({ startDate, endDate })
+  const endDateWithAdvanceNotice = addDays(endDate, days)
+
+  const grossValue = roundCurrency(grossSalary * days / 30)
+  const fgts = roundCurrency(grossValue * 0.08)
+  const inss = INSS(grossValue)
+  const irrf = 0
+  const netValue = grossValue - inss - irrf
+
+  return {
+    grossValue,
+    fgts,
+    inss,
+    irrf,
+    netValue,
+    details: {
+      advanceNoticeDays: days,
+      endDateWithAdvanceNotice
+    }
+  }
+}
+
+function advanceNoticeDays ({ startDate, endDate }) {
+  return 30 + 3 * completedYears(startDate, endDate)
+}
+
 module.exports = {
-  salaryRemainer
+  salaryRemainer,
+  advanceNoticeSalary
 }
--- src/payments.test.js
+++ src/payments.test.js
@@ -1,7 +1,10 @@
 describe('Payment Functions', function () {
   const { expect } = require('chai')
 
-  const { salaryRemainer } = require('./payments')
+  const {
+    salaryRemainer,
+    advanceNoticeSalary
+  } = require('./payments')
 
   const salaryRemainerTests = [
     {
@@ -23,6 +26,32 @@ describe('Payment Functions', function () {
   salaryRemainerTests.forEach(function ({ grossSalary, endDate, expectedResult }) {
     it(`calculates salary remainer for ${grossSalary} gross salary and termination date ${endDate}`, function () {
       const result = salaryRemainer({ grossSalary, endDate })
+      expect(result).to.eql(expectedResult)
+    })
+  })
+
+  const advanceNoticeSalaryTests = [
+    {
+      grossSalary: 5000,
+      startDate: '2014-11-06',
+      endDate: '2020-10-12',
+      expectedResult: {
+        grossValue: 7500,
+        fgts: 600,
+        inss: 713.09,
+        irrf: 0,
+        netValue: 6786.91,
+        details: {
+          advanceNoticeDays: 45,
+          endDateWithAdvanceNotice: '2020-11-26'
+        }
+      }
+    }
+  ]
+
+  advanceNoticeSalaryTests.forEach(function ({ grossSalary, startDate, endDate, expectedResult }) {
+    it(`calculates advance notice salary for ${grossSalary} gross salary, working between ${startDate} and ${endDate}`, function () {
+      const result = advanceNoticeSalary({ grossSalary, startDate, endDate })
       expect(result).to.eql(expectedResult)
     })
   })
