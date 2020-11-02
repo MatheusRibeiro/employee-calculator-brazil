b7962da111dbc9fc6cfc8d1ffa32d5f35cd7ba62
Matheus Ribeiro <matheus.salgado@gmail.com>
2020-11-02 13:17:23 -0300

    refactoring add days module so it can be easier to reuse internally its own functions

--- src/dateHelper.js
+++ src/dateHelper.js
@@ -1,36 +1,43 @@
 const moment = require('moment')
 
-module.exports = {
-  completedYears: function completedYears (startDate, endDate) {
-    const startsAt = moment(`${startDate} 00:00`)
-    const endsAt = moment(`${endDate} 23:59`)
-    return endsAt.diff(startsAt, 'years')
-  },
-  completedMonthsFromAniversary: function completedMonthsFromAniversary (startDate, endDate) {
-    const startsAt = moment(`${startDate} 00:00`)
-    const endsAt = moment(`${endDate} 23:59`)
-    return endsAt.diff(startsAt, 'months') % 12
-  },
+function completedYears (startDate, endDate) {
+  const startsAt = moment(`${startDate} 00:00`)
+  const endsAt = moment(`${endDate} 23:59`)
+  return endsAt.diff(startsAt, 'years')
+}
 
-  completedMonthsFromYear: function completedMonthsFromYear (referenceDate) {
-    const date = moment(referenceDate)
-    const month = date.month()
-    const day = date.date()
+function completedMonthsFromAniversary (startDate, endDate) {
+  const startsAt = moment(`${startDate} 00:00`)
+  const endsAt = moment(`${endDate} 23:59`)
+  return endsAt.diff(startsAt, 'months') % 12
+}
 
-    if (day >= 15) {
-      return month + 1
-    }
-    return month
-  },
+function completedMonthsFromYear (referenceDate) {
+  const date = moment(referenceDate)
+  const month = date.month()
+  const day = date.date()
 
-  completedDaysFromMonth: function completedDaysFromMonth (referenceDate) {
-    const date = moment(referenceDate)
-    return date.date()
-  },
-
-  addDays: function addDays (referenceDate, daysToAdd) {
-    return moment(referenceDate)
-      .add(daysToAdd, 'days')
-      .format('YYYY-MM-DD')
+  if (day >= 15) {
+    return month + 1
   }
+  return month
+}
+
+function completedDaysFromMonth (referenceDate) {
+  const date = moment(referenceDate)
+  return date.date()
+}
+
+function addDays (referenceDate, daysToAdd) {
+  return moment(referenceDate)
+    .add(daysToAdd, 'days')
+    .format('YYYY-MM-DD')
+}
+
+module.exports = {
+  completedYears,
+  completedMonthsFromAniversary,
+  completedMonthsFromYear,
+  completedDaysFromMonth,
+  addDays
 }
